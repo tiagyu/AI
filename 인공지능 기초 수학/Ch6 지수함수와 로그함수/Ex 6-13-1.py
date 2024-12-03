ea1515da6cb76043b384e6cf855906b6ec9a4750
tiagyu <tkroh1997@gmail.com>
2024-12-03 21:55:05 +0900

    Create Ex 6-13-1.py

---
+++ 인공지능 기초 수학/Ch6 지수함수와 로그함수/Ex 6-13-1.py
@@ -0,0 +1,9 @@
+# 파이썬 math 라이브러리를 호출
+import math
+
+# log 값을 얻으려고 math 라이브러리의 log() 함수를 사용
+log = math.log(2,4) # 밑이 4고 진수가 2인 로그
+print(log)
+
+log2 = math.log(4,2) # 밑이 2이고 진수가 4인 로그
+print(log2)
