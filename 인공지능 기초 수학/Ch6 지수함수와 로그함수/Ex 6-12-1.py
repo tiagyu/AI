145ced9d11063c1776c046ce1f138c1cc7c26f59
tiagyu <tkroh1997@gmail.com>
2024-12-03 21:40:59 +0900

    Update Ex 6-12-1.py

--- 인공지능 기초 수학/Ch6 지수함수와 로그함수/Ex 6-12-1.py
+++ 인공지능 기초 수학/Ch6 지수함수와 로그함수/Ex 6-12-1.py
@@ -2,3 +2,14 @@
 import math
 
 # 파이썬에서 제공하는 지수함수를 유형별로 실행
+# 2의 5 제곱
+pow = math.pow(2,5)
+print('pow 결과 :', pow)
+
+# 2의 제곱근
+sqrt = math.sqrt(2)
+print('sqrt 결과 :', sqrt)
+
+# e의 2제곱
+exp = math.exp(2)
+print('exp 결과 :', exp)
