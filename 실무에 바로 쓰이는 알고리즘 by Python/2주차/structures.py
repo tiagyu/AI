b3d32289fbe359c089b8abe035b99e13e0b0009d
tiagyu <tkroh1997@gmail.com>
2024-12-05 23:39:22 +0900

    Update

---
+++ 실무에 바로 쓰이는 알고리즘 by Python/2주차/structures.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, val):
+        if not self.head:
+            self.head = ListNode(val, None)
+            return
+
+        node = self.head
+        while node.next:
+            node = node.next
+
+        node.next = ListNode(val, None)
+
+a = LinkedList()
+
+a.append(5)
+a.append(7)
+
+print(a)
