5953c7dfad83d15486e1c97fb298ba13d6d0c8d6
tiagyu <tkroh1997@gmail.com>
2025-01-01 23:28:03 +0900

    Update 9-7 메소드 오버라이딩.py

--- 파이썬 문법 공부/9. 클래스/9-7 메소드 오버라이딩.py
+++ 파이썬 문법 공부/9. 클래스/9-7 메소드 오버라이딩.py
@@ -1,14 +1,19 @@
 # 메소드 오버라이딩
 # 일반 유닛
 class Unit:
-    def __init__(self, name, hp):
+    def __init__(self, name, hp, speed):
         self.name = name
         self.hp = hp
+        self.speed = speed
+        
+    def move(self, location):
+        print('[지상 유닛 이동]')
+        print(f'{self.name} : {location} 방향으로 이동합니다. [속도 {self.speed}]')
 
 # 공격 유닛
 class AttackUnit(Unit):
-    def __init__(self, name, hp, damage):
-        Unit.__init__(self, name, hp) # Unit class를 상속 받음
+    def __init__(self, name, hp, speed, damage):
+        Unit.__init__(self, name,hp, speed) # Unit class를 상속 받음
         self.damage = damage
         
     def attack(self, location):
@@ -34,11 +39,19 @@ class Flyable:
 # 공중 공격 유닛 클래스
 class FlyableAttackUnit(AttackUnit, Flyable):
     def __init__(self, name, hp, damage, flying_speed):
-        AttackUnit.__init__(self, name, hp, damage)
+        AttackUnit.__init__(self, name, hp, 0,damage) # 지상 speed는 0
         Flyable.__init__(self, flying_speed)
         
-# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사
-valkyrie = FlyableAttackUnit('발키리',200,6,5)
-valkyrie.fly(valkyrie.name, '3시')
+    def move(self, location)   :
+        print('[공중 유닛 이동]')
+        self.fly(self.name, location)
+        
+# 벌처 : 지상 유닛, 기동성이 좋음
+vulture = AttackUnit('벌처', 80, 10, 20)
 
-# 메소드 오버라이딩
+# 배틀크루저 : 공중 유닛
+battlecruiser = FlyableAttackUnit('배틀크루저', 500, 25, 3)
+
+vulture.move('11시')
+# battlecruiser.fly(battlecruiser.name, '9시')
+battlecruiser.move('9시')
