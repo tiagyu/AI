2e43b452e62956d096135f6f1a2645f9c00e9080
tiagyu <tkroh1997@gmail.com>
2025-01-03 23:35:13 +0900

    Create 9-10 스타크래프트 전반전.py

---
+++ 파이썬 문법 공부/9. 클래스/9-10 스타크래프트 전반전.py
@@ -0,0 +1,8 @@
+# 스타크래프트
+# 일반 유닛
+class Unit:
+    def __init__(self, name, hp, speed):
+        # __init__ 매서드는 객체를 생성하고 초기화하는 역할을 수행
+        self.name = name
+        self.hp = hp
+        self.speed = speed
