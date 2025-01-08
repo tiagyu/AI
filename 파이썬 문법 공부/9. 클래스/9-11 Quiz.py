3d8509ef03d97bd56c76242c22cbc7bf606d5eb3
tiagyu <tkroh1997@gmail.com>
2025-01-08 23:16:30 +0900

    Create 9-11 Quiz.py

---
+++ 파이썬 문법 공부/9. 클래스/9-11 Quiz.py
@@ -0,0 +1,19 @@
+"""
+Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오
+
+(출력 예제)
+총 3대의 매물이 있습니다
+강남 아파트 매매 10억 2010년
+마포 오피스텔 전세 5억 2007년
+송파 빌라 월세 500/50 2000년
+"""
+
+# 코드
+class House:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, complete_year):
+        pass
+    
+    # 매물 정보 표시
+    def detail(self):
+        pass
