33edf67047146ed7fde8249ba8e012adff61a97e
tiagyu <tkroh1997@gmail.com>
2025-01-09 23:01:45 +0900

    Create pratice_module.py

---
+++ 파이썬 문법 공부/11. 모듈과 패키지/pratice_module.py
@@ -0,0 +1,15 @@
+# 모듈
+"""
+class, def를 담고있는 파일을 모듈이라고 한다
+"""
+# 일반 가격
+def popcorn(price):
+    print(f'5명 팝콘의 가격은 {int(price * 5)}원 입니다')
+    
+# 쿠폰 할인 가격
+def coupon(price):
+    print(f'5명 팝콘의 가격은 {int((price * 5) * 0.8)}원 입니다')
+    
+# 군인 할인 가격
+def soldier(price):
+    print(f'5명 팝콘의 가격은 {int((price * 5) * 0.5)}원 입니다')
