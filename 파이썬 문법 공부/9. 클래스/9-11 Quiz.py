71e87adee0bf49e5a3dc1383529697855ba0fa5f
tiagyu <tkroh1997@gmail.com>
2025-01-16 22:31:19 +0900

    Update 9-11 Quiz.py

--- 파이썬 문법 공부/9. 클래스/9-11 Quiz.py
+++ 파이썬 문법 공부/9. 클래스/9-11 Quiz.py
@@ -12,8 +12,24 @@ Quiz) 주어진 코드를 활용하여 부동산 프로그램을 작성하시오
 class House:
     # 매물 초기화
     def __init__(self, location, house_type, deal_type, price, complete_year):
-        pass
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.complete_year = complete_year
     
-    # 매물 정보 표시
+# 매물 정보 표시
     def detail(self):
-        pass
+        print(f'{self.location} {self.house_type} {self.deal_type} {self.price} {self.complete_year}')
+
+# House 인스턴스 생성
+houses = [
+    House("강남", "아파트", "매매", "10억", "2010년"),
+    House("마포", "오피스텔", "전세", "5억", "2007년"),
+    House("송파", "빌라", "월세", "500/50", "2000년"),
+]
+
+# 매물 정보 출력
+print(f"총 {len(houses)}대의 매물이 있습니다.")
+for house in houses:
+    house.detail()
