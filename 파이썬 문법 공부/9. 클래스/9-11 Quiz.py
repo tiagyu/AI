b4d67554c2312f1a4a5ff26beb85a452712c247c
tiagyu <tkroh1997@gmail.com>
2025-01-17 23:43:45 +0900

    Update 9-11 Quiz.py

--- 파이썬 문법 공부/9. 클래스/9-11 Quiz.py
+++ 파이썬 문법 공부/9. 클래스/9-11 Quiz.py
@@ -33,3 +33,28 @@ houses = [
 print(f"총 {len(houses)}대의 매물이 있습니다.")
 for house in houses:
     house.detail()
+    
+## 강의 코드
+class House1:
+    # 매물 초기화
+    def __init__(self, location, house_type, deal_type, price, complete_year):
+        self.location = location
+        self.house_type = house_type
+        self.deal_type = deal_type
+        self.price = price
+        self.complete_year = complete_year
+
+    def show_detail(self):
+        print(self.location, self.house_type, self.deal_type, self.price, self.complete_year)
+
+house = []
+house1 = House1('강남','아파트','매매','10억','2010년')
+house2 = House1("마포", "오피스텔", "전세", "5억", "2007년")
+house3 = House1("송파", "빌라", "월세", "500/50", "2000년")
+house.append(house1)
+house.append(house2)
+house.append(house3)
+
+print(f'총 {len(house)}대의 매물이 있습니다.')
+for home in house:
+    home.show_detail()
