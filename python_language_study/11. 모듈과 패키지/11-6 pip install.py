a8bc91840a3ae8007b58bef40bd10be33a2300d3
tiagyu <tkroh1997@gmail.com>
2025-01-28 02:25:20 +0900

    Create 11-6 pip install.py

---
+++ python_language_study/11. 모듈과 패키지/11-6 pip install.py
@@ -0,0 +1,8 @@
+"""
+1) pip install : 패키지 설치 명령어
+2) pip list : 설치된 패키지 볼 수 있는 명령어
+3) pip show python : python 패키지에 대한 내용이 나오는 명령어
+4) pip install --upgrade python : python 버전 업그레이드 하는 명령어
+5) pip uninstall python : python 패키지 삭제하는 명령어
+
+"""
