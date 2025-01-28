f1a638a4dabb5b5b06abba5a108265e2a6f666bd
tiagyu <tkroh1997@gmail.com>
2025-01-29 00:00:52 +0900

    Create printfscanf.c

---
+++ C_language_study/printfscanf.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+int main(void)
+{   
+    // 정수형 변수에 대한 예제 (한줄줄)
+    /*int age = 12;
+    printf("%d\n",age);
+    age=13;
+    printf("%d\n",age); 여러 줄 주석 처리리
+    */
+
+    // 실수형 변수에 대한 예제
+    float f = 46.5f;
+    printf("%.2f\n",f);
+    double d = 4.428;
+    printf("%.2lf\n", d);
+    return 0;
+}
