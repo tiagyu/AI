71476db698910aaea21e84a0ba64fc933243ee62
tiagyu <tkroh1997@gmail.com>
2025-03-12 01:18:28 +0900

    Create 3_shorts_scripts_generator.py

---
+++ Streamlit_study/Coding_School/my-chatgpt-service/pages/3_shorts_scripts_generator.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "chatGPT API 서비스 개발",
+    page_icon = "https://static.vecteezy.com/system/resources/thumbnails/021/059/827/small_2x/chatgpt-logo-chat-gpt-icon-on-white-background-free-vector.jpg"
+)
+
+st.title("AI 유튜브 쇼츠 대본 생성기")
