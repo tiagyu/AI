f84e82bb67eb6e5d6d2f2020c5aa881f94598a89
tiagyu <tkroh1997@gmail.com>
2025-03-12 01:18:02 +0900

    Create app.py

---
+++ Streamlit_study/Coding_School/my-chatgpt-service/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "chatGPT API 서비스 개발",
+    page_icon = "https://static.vecteezy.com/system/resources/thumbnails/021/059/827/small_2x/chatgpt-logo-chat-gpt-icon-on-white-background-free-vector.jpg"
+)
+
+st.title("chatgpt API로 AI 서비스 개발하기")
+st.subheader("다양한 서비스를 만들어보자")
